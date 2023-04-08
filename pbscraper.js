26ebe87a6278fdb4373b4d8779414a42fb0a3647
roy nsumba <83820529+roynsumba@users.noreply.github.com>
2023-04-08 19:19:51 +0300

    fifth commit

--- pbscraper.js
+++ pbscraper.js
@@ -41,6 +41,11 @@ app.post("/search", async (req, res) => {
   }
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong.");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
